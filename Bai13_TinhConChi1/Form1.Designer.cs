9230a2039c305c5b051830b55dc64ae5c6728625
congialap5jack <kietcuchi148@gmail.com>
2025-02-24 15:49:44 +0700

    thêm ghi chú

--- Bai13_TinhConChi1/Form1.Designer.cs
+++ Bai13_TinhConChi1/Form1.Designer.cs
@@ -28,28 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CanChi = new System.Windows.Forms.TextBox();
+            this.chiArr = new System.Windows.Forms.TextBox();
             this.btnTinh = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtNam = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.Label();
+            this.lstNam = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // CanChi
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(171, 20);
-            this.textBox1.TabIndex = 0;
+            this.CanChi.Location = new System.Drawing.Point(187, 36);
+            this.CanChi.Name = "CanChi";
+            this.CanChi.Size = new System.Drawing.Size(171, 20);
+            this.CanChi.TabIndex = 0;
             // 
-            // textBox2
+            // chiArr
             // 
-            this.textBox2.Location = new System.Drawing.Point(187, 90);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(171, 20);
-            this.textBox2.TabIndex = 0;
+            this.chiArr.Location = new System.Drawing.Point(187, 90);
+            this.chiArr.Name = "chiArr";
+            this.chiArr.Size = new System.Drawing.Size(171, 20);
+            this.chiArr.TabIndex = 0;
             // 
             // btnTinh
             // 
@@ -57,56 +57,57 @@
             this.btnTinh.Name = "btnTinh";
             this.btnTinh.Size = new System.Drawing.Size(74, 23);
             this.btnTinh.TabIndex = 1;
-            this.btnTinh.Text = "button1";
+            this.btnTinh.Text = "Tinh";
             this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
             // 
-            // button2
+            // btnExit
             // 
-            this.button2.Location = new System.Drawing.Point(395, 88);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(74, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnExit.Location = new System.Drawing.Point(395, 88);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // txtNam
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 39);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "NĂM:";
+            this.txtNam.AutoSize = true;
+            this.txtNam.Location = new System.Drawing.Point(116, 39);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(34, 13);
+            this.txtNam.TabIndex = 2;
+            this.txtNam.Text = "NĂM:";
             // 
-            // label2
+            // txtKQ
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "KẾT QUẢ";
+            this.txtKQ.AutoSize = true;
+            this.txtKQ.Location = new System.Drawing.Point(116, 93);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(54, 13);
+            this.txtKQ.TabIndex = 2;
+            this.txtKQ.Text = "KẾT QUẢ";
             // 
-            // listBox1
+            // lstNam
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(545, 56);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(224, 277);
-            this.listBox1.TabIndex = 3;
+            this.lstNam.FormattingEnabled = true;
+            this.lstNam.Location = new System.Drawing.Point(545, 56);
+            this.lstNam.Name = "lstNam";
+            this.lstNam.Size = new System.Drawing.Size(224, 277);
+            this.lstNam.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lstNam);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnTinh);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chiArr);
+            this.Controls.Add(this.CanChi);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -116,13 +117,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox CanChi;
+        private System.Windows.Forms.TextBox chiArr;
         private System.Windows.Forms.Button btnTinh;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label txtNam;
+        private System.Windows.Forms.Label txtKQ;
+        private System.Windows.Forms.ListBox lstNam;
     }
 }
 
--- Bai13_TinhConChi1/Form1.cs
+++ Bai13_TinhConChi1/Form1.cs
@@ -16,5 +16,24 @@ namespace Bai13_TinhConChi1
         {
             InitializeComponent();
         }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            string[] canArr = { "canh ", "tân ", "Nhâm ", "quý ", "giáp ", "ất ", "bính ", "đinh ", "mậu ", "kỷ " };
+            string[] chiArr = { "thân ", "dậu ", "tuất ", "hợi ", "tý ", "sửu ", "dần ", "mão ", "thìn ", "tị ", "ngọ ", "mùi " };
+            string giatritrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + "" + chiArr[Convert.ToInt32(txtNam.Text) % 12];
+            txtKQ.Text = giatritrave;
+
+            int startYear = 1900;
+            int endYear = 2100;
+            for (int i = startYear; i <= endYear; i++)
+            {
+                lstNam.Items.Add(i + " - " + canArr[i % 10] + " " + chiArr[i % 12]);
+            }
+        }
+        
+
+            
+        
     }
 }
